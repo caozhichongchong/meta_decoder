15e685f9829c2e591b9622c63d45b171733cfc45
caozhichongchong <caozhichongchong@gmail.com>
2020-10-08 13:29:10 -0400

    visualize heatmap by python

--- heatmap.R
+++ heatmap.R
@@ -1,14 +1,20 @@
-Allele1=read.delim('all.snp.profile',header=F)
-Ref='GACCAATTGACAACAACAAAGACTCATCCGAGCTTTAAAGCCGGTGATAATATTACCGTTAACTACAAGATCGTAGAGGGTGGTAAAGAGCGTATCCAGAGTTTCCGTGGAGATGTGATCAAAACACAGGGAAATGGTGCTACTGCATCTTTCACTGTACGTAAGATCTCTGATGGAGTAGGTGTAGAACGTTTGTTCCCTATCCTTTCTCCAAATATCGACTCTATCGTATTTAACAAAGCCGGTAAATTTCGTCGCGCTAAATTATACTACCTGCGTGACCGCAGCGGTAAGAGCGCACGTATCAAGGAAAAA'
-Allele1=Allele1[!duplicated(paste(Allele1$V1,Allele1$V2)),]
+#inputpath = 'example'
+Allele1=read.delim(paste(inputpath,'all.snp.profile',sep = '/'),header=F)
+Allele1=Allele1[!duplicated(paste(Allele1$V1,Allele1$V3)),]
+Allele1 = Allele1[which(Allele1$V2!=''),]
 library(stringr)
 Allele1$allele=str_split_fixed(Allele1$V1, "--", 2)[,2]
 Allele1$position=str_split_fixed(Allele1$allele, "_", 2)[,1]
 Allele1$allele=str_split_fixed(Allele1$allele, "_", 2)[,2]
-Allele1$genotypes = paste(str_split_fixed(Allele1$V2, "_", 2)[,1],
-                          str_split_fixed(Allele1$V2, ":", 2)[,2],
+Allele1$genotypes = paste(str_split_fixed(Allele1$V3, "_", 2)[,1],
+                          str_split_fixed(Allele1$V3, ":", 2)[,2],
                           sep = ':')
-genotypes = unique(Allele1$genotypes)
+Allele1$refgenome = str_split_fixed(Allele1$V1, "_", 2)[,1]
+Alleleall = Allele1
+allgenome = unique(Alleleall$refgenome)
+for (genome in allgenome)
+{Allele1=Alleleall[which(Alleleall$refgenome == genome),]
+  genotypes = unique(Allele1$genotypes)
 position = unique(Allele1$position)
 Allele = matrix('ref',ncol=length(genotypes),
                 nrow=length(position))
@@ -16,25 +22,18 @@ colnames(Allele)=genotypes
 row.names(Allele)=position
 for(i in 1:nrow(Allele1))
 {
-  n=which(colnames(Allele)==Allele1$genotypes[i])
+  if(Allele1$V2[i]!=toString(Allele1$allele[i]))
+  {n=which(colnames(Allele)==Allele1$genotypes[i])
   m=which(row.names(Allele)==Allele1$position[i])
-  Allele[m,n]=Allele1$allele[i]
+  Allele[m,n]=Allele1$allele[i]}
 }
-nchar(Ref)
-temp = 234
-substr(Ref, temp, temp)
-Allele_correct=Allele
-for(i in 1:nrow(Allele))
-{for (j in 1:ncol(Allele))
-{
-  if(Allele_correct[i,j]=='ref')
-    Allele_correct[i,j]=substr(Ref, as.numeric(row.names(Allele_correct)[i]), 
-                               as.numeric(row.names(Allele_correct)[i]))
-}}
 
 library(pheatmap)
 library(RColorBrewer)
 mat= Allele
+Color_set = c('#e5e5e5','#ca0020',
+              '#f4a582','#92c5de','#0571b0')
+
 mat[which(mat=='ref')]=0
 mat[which(mat=='A')]=1
 mat[which(mat=='T')]=2
@@ -44,10 +43,12 @@ mat=matrix(as.numeric(mat),nrow=nrow(mat))
 row.names(mat)=row.names(Allele)
 colnames(mat)=colnames(Allele)
 mat=mat[order(as.numeric(row.names(mat))),]
-
 pheatmap(mat= mat,
-         color = colorRampPalette((brewer.pal(n = 8, name = "Purples")))(5),
+         color = Color_set,
          show_rownames= TRUE,
          show_colnames = TRUE,
          cluster_cols = TRUE,
-         cluster_rows = FALSE)
+         legend_labels = c('Major allele','A','T','G','C'),
+         filename = paste(inputpath,paste(genome,'snp.profile.SNP.pdf',sep = '.'),sep = '/'),
+         cluster_rows = FALSE)}
+
--- meta_decoder.sum.py
+++ meta_decoder.sum.py
@@ -1,15 +1,25 @@
 import glob
 import os
 import argparse
+import subprocess
 
 # Arguments and declarations
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
 parser.add_argument("-i",
                     help="input dir of vcfs output by meta_decoder", type=str,
                     default='result_decoder',metavar='result_decoder')
+parser.add_argument("-fq",
+                    help="file extension of fastq #1 files", type=str,
+                    default='_1.fastq',metavar='_1.fastq')
+parser.add_argument('-R',
+                          help="Optional: complete path to R if not in PATH",
+                          metavar="/usr/local/bin/R",
+                          action='store', default='R', type=str)
 args = parser.parse_args()
 
+workingdir=os.path.abspath(os.path.dirname(__file__))
 # function
-def freq_call_sub(vcf_file):
+def freq_call_sub(vcf_file,REF):
     SNP = dict()
     SNP_count = dict()
     Total = 0
@@ -18,7 +28,6 @@ def freq_call_sub(vcf_file):
         if not lines.startswith("#"):
             lines_set = lines.split('\t')
             Depth = int(lines_set[7].split('DP=')[1].split(';')[0])
-            print(vcf_file,Depth)
             Chr = lines_set[0]
             SNP_count.setdefault(Chr, 0)
             Position = int(lines_set[1])
@@ -39,7 +48,6 @@ def freq_call_sub(vcf_file):
                     allels_set = lines_set[4].split(',')
                     for allels in allels_set:
                         if allels in Allels:
-                            print('multiple')
                             Allels_frq[Allels[allels]] += int(Sum_alt * 0.5)
                         else:
                             pass
@@ -48,6 +56,7 @@ def freq_call_sub(vcf_file):
                         temp += 1
                 if temp >= 2:
                     SNP.setdefault(Chr_position, Allels_frq)
+                    REF.setdefault(Chr_position, lines_set[3])
                     SNP_count[Chr] += 1
                     if Sum_ref < Sum_alt:
                         SNP_major.setdefault(Chr_position, [lines_set[4],
@@ -55,13 +64,14 @@ def freq_call_sub(vcf_file):
                     else:
                         SNP_major.setdefault(Chr_position, [lines_set[3],
                                                             lines_set[4]])
-    return [SNP, SNP_count, SNP_major, Total]
+    return [SNP, SNP_count, SNP_major, Total,REF]
 
 # set up
 vcf = glob.glob(os.path.join(args.i,'*flt.vcf'))
 snp_profile = dict()
 snp_number = dict()
-snp_profile_sample = dict()
+REF = dict()
+#snp_profile_sample = dict()
 Set=['major','minor']
 Allels = dict()
 Allels['A']=0
@@ -69,37 +79,53 @@ Allels['T']=1
 Allels['G']=2
 Allels['C']=3
 Allels_order = ['A','T','G','C']
-
+print('run example: python meta_decoder.sum.py -i example/ -fq 1.fastq -R R')
 for files in vcf:
-  SNP,SNP_count,SNP_major,Total = freq_call_sub(files)
-  samplename = files.split('.fa.blast.txt.filter.aa.flt.vcf')[0]
-  for Chr_i in SNP_major:
-    temp = 0
-    for major in SNP_major[Chr_i]:
-        snp_profile.setdefault('%s_%s' % (Chr_i, major), [])
-        snp_profile['%s_%s' % (Chr_i, major)].append(samplename + ':' + Set[temp])
-        snp_profile_sample.setdefault(samplename, [])
-        snp_profile_sample[samplename].append(['%s_%s' % (Chr_i, major), SNP[Chr_i][Allels[major]]])
-        temp += 1
-  for Chr in SNP_count:
-    snp_number.setdefault(samplename,SNP_count[Chr])
+    print('processing vcf %s'%(files))
+    SNP, SNP_count, SNP_major, Total, REF = freq_call_sub(files, REF)
+    samplename = os.path.split(files)[-1].split(args.fq)[0]
+    i = 0
+    for Chr_i in SNP_major:
+        temp = 0
+        for major in SNP_major[Chr_i]:
+            snp_profile.setdefault('%s_%s' % (Chr_i, major), [])
+            snp_profile['%s_%s' % (Chr_i, major)].append(samplename + ':' + Set[temp])
+            # snp_profile_sample.setdefault(samplename, [])
+            # snp_profile_sample[samplename].append(['%s_%s' % (Chr_i, major), SNP[Chr_i][Allels[major]]])
+            temp += 1
+        i += 1
+    for Chr in SNP_count:
+        snp_number.setdefault(samplename, SNP_count[Chr])
 
+print('output all snp numbers %s'%(os.path.join(args.i,'all.snp.number')))
 f1=open(os.path.join(args.i,'all.snp.number'),'w')
 for samplename in snp_number:
   f1.write('%s\t%s\n'%(samplename,snp_number[samplename]))
 
 f1.close()
 
+print('output all snp profiles %s'%(os.path.join(args.i,'all.snp.profile')))
 f1=open(os.path.join(args.i,'all.snp.profile'),'w')
 for Chr_i in snp_profile:
-  f1.write('%s\t%s\n'%(Chr_i,'\t'.join(snp_profile[Chr_i])))
+  f1.write('%s\t%s\t%s\n'%(Chr_i,REF['_'.join(Chr_i.split('_')[:-1])],'\t'.join(snp_profile[Chr_i])))
 
 f1.close()
 
-f1=open(os.path.join(args.i,'all.snp.profile.sample'),'w')
-for position in snp_profile_sample:
-    for Chr_i_major in snp_profile_sample[position]:
-            for samplename_allele in snp_profile[Chr_i_major[0]]:
-                f1.write('%s\t%s\n'%(Chr_i_major[0],(samplename_allele)))
-
+# run heatmap.R
+print('generating heatmap.R')
+newRcode_temp = os.path.join(args.i,'heatmap_temp.R')
+newRcode = os.path.join(args.i,'heatmap.R')
+f1 = open(newRcode_temp,'w')
+f1.write('inputpath = \'%s/\'\n'%(args.i))
 f1.close()
+os.system('cat %s %s > %s'%(newRcode_temp,os.path.join(workingdir,'heatmap.R'),newRcode))
+print('plot all snp profiles %s/*.snp.profile.SNP.pdf'%(args.i))
+os.system('%s --vanilla < %s'%(args.R, newRcode))
+
+#f1=open(os.path.join(args.i,'all.snp.profile.sample'),'w')
+#for position in snp_profile_sample:
+#    for Chr_i_major in snp_profile_sample[position]:
+#            for samplename_allele in snp_profile[Chr_i_major[0]]:
+#                f1.write('%s\t%s\n'%(Chr_i_major[0],(samplename_allele)))
+
+#f1.close()
